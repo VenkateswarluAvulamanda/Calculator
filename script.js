e72d4d5a9f1015d1b1731cdb99c6525e9d54697d
Venkateswarlu Avulamanda <venkatesh78199@gmail.com>
2025-03-14 11:53:11 +0530

    Updated script for better calculations

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ function clearData() {
 }
 function calculateValue(value) {
     let input = document.getElementById('input');
+    if(input.value.includes("Invalid")) {
+        input.value = '';
+        return;
+    }
     input.value = input.value + value;
 }
 function clearLastData() {
     let input = document.getElementById('input');
+    if(input.value.includes("Invalid")) {
+        input.value = '';
+        return;
+    }
     input.value = input.value.substring(0,input.value.length-1);
 }
 function calculateResult() {
     let input = document.getElementById('input');
+    input.value = input.value.replace(/^0+/, "");
     try {
         input.value = eval(input.value);
+        if(input.value == 'undefined') {
+            input.value = '';
+        }
     } catch {
         input.value = 'Invalid'
     }
